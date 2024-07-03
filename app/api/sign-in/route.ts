904c8ebe53acf0e6fb6df9796b30b83e5efd1a3f
Digzcod <stephenjaramillo5@gmail.com>
2024-07-03 11:21:15 +0800

    route sign-up try  fix issue

--- app/api/sign-in/route.ts
+++ app/api/sign-in/route.ts
@@ -1,41 +1,41 @@
 
 
-// import { NextRequest, NextResponse } from "next/server";
-// import createClientServer from "@/utils/supabase/server";
-// import { setCookie } from "cookies-next";
+import { NextRequest, NextResponse } from "next/server";
+import createClientServer from "@/utils/supabase/server";
+import { setCookie } from "cookies-next";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-// export async function POST(req: NextRequest) {
-//   const supabase = createClientServer();
-//   const { email, password, remember } = await req.json();
-//   const { data, error } = await supabase.auth.signInWithPassword({
-//     email,
-//     password,
-//   });
+export async function POST(req: NextRequest) {
+  const supabase = createClientServer();
+  const { email, password, remember } = await req.json();
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
 
-//   if (error) {
-//     return NextResponse.json({ message: error.message }, { status: 401 });
-//   }
-//   // Calculate maxAge or user login session based on the `remember` boolean
-//   const maxAge = remember ? 30 * 24 * 60 * 60 : 5 * 60; // 30 days longer login or 5 minutes
+  if (error) {
+    return NextResponse.json({ message: error.message }, { status: 401 });
+  }
+  // Calculate maxAge or user login session based on the `remember` boolean
+  const maxAge = remember ? 30 * 24 * 60 * 60 : 5 * 60; // 30 days longer login or 5 minutes
 
-//   // Create a response object to manipulate headers
-//   const response = NextResponse.json(
-//     { message: "Sign in successful", data },
-//     { status: 200 }
-//   );
+  // Create a response object to manipulate headers
+  const response = NextResponse.json(
+    { message: "Sign in successful", data },
+    { status: 200 }
+  );
 
-//   // Set cookie with proper response object
-//   setCookie("user", JSON.stringify(data.user), {
-//     req,
-//     res: response, // Pass the response object here
-//     maxAge,
-//     path: "/",
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === "production",
-//     sameSite: "lax",
-//   });
+  // Set cookie with proper response object
+  setCookie("user", JSON.stringify(data.user), {
+    req,
+    res: response, // Pass the response object here
+    maxAge,
+    path: "/",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
 
-//   return response;
-// }
+  return response;
+}
