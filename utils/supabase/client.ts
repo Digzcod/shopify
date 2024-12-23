f61f127e79fc128c8e2ed438f4413f5f09a6f802
Digzcod <stephenjaramillo@gmail.com>
2024-12-23 09:42:35 +0800

    update nextv18.2.21 reactv18.3.1

--- utils/supabase/client.ts
+++
@@ -1,9 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-function createClient() {
-  const supabase_url = process.env.SUPABASE_URL as string;
-  const supabase_key = process.env.SUPABASE_ANON_KEY as string;
-  return createBrowserClient(supabase_url, supabase_key);
-}
-
-export default createClient;
